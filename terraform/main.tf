33d227df7c432f218489878d1bef5a8496d9a7b3
Rakib Mustafa <rakib.mustafa.com>
2024-07-10 19:24:58 +0000

    Fix duplicate provider and resource configurations in Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,16 +29,4 @@ resource "aws_instance" "example" {
     Name = "ExampleInstance"
   }
 }
-provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-09b5c6390225b29cc"  # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleInstance"
-  }
-}
 
